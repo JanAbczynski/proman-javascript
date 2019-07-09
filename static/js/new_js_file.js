4f58fb044ffb39f1031acb81c63d409249f1d404
weronikawalczak <weronikawalczak989@gmail.com>
2019-07-09 14:48:03 +0200

    dragula

--- static/js/new_js_file.js
+++ static/js/new_js_file.js
@@ -1,4 +1,11 @@
 ////////HIDING CERTAIN CARD ELEMENTS
+
+let board_len = document.getElementsByClassName('my_class').length;
+
+for (let i=0; i<board_len; i++){
+    dragula([document.getElementsByClassName('left')[i], document.getElementsByClassName('right')[i],  document.getElementsByClassName('right2')[i],  document.getElementsByClassName('left2')[i]]);
+}
+
 let btnHide = document.getElementById('btn-hide');
 let hideDiv = document.getElementById('to-hide');
 const createCard = function () {
